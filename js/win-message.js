b2cfb2b91168662a9e504f10affc45050a71f7ec
manateebait <59706500+manateebait@users.noreply.github.com>
2025-05-23 09:53:44 -0400

    Update win-message.js

--- js/win-message.js
+++ js/win-message.js
@@ -1,1 +1,1 @@
-const WIN_MESSAGE = "🎉 You guessed it! 🎉 </br> Welcome Emmett William-Chandler Stratton </br> born July 20th, 2025! </br> <a href="https://photos.app.goo.gl/BnxmZWCUtiwaN9cN7">See the baby!</a>";
+const WIN_MESSAGE = "🎉 You guessed it! 🎉 <br> Welcome Emmett William-Chandler Stratton <br> born July 20th, 2025! <br> <a href='https://photos.app.goo.gl/BnxmZWCUtiwaN9cN7' target='_blank'>See the baby!</a>";
