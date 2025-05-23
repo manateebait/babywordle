1923b53f8568711ef36e9e312940b8b78e4b7d71
manateebait <59706500+manateebait@users.noreply.github.com>
2025-05-23 09:44:20 -0400

    Create win-message.js

---
+++ js/win-message.js
@@ -0,0 +1,1 @@
+const WIN_MESSAGE = "🎉 You guessed it! 🎉 </br> Welcome Emmett William-Chandler Stratton </br> born July 20th, 2025! </br> <a href="https://photos.app.goo.gl/BnxmZWCUtiwaN9cN7">See the baby!</a>";
