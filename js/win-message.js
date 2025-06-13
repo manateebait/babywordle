c4f69f84eaa8bdb185deb8d2571746a1dda380e3
manateebait <59706500+manateebait@users.noreply.github.com>
2025-06-13 11:20:34 -0400

    Update win-message.js

--- js/win-message.js
+++ js/win-message.js
@@ -1,7 +1,7 @@
 const WIN_MESSAGE = `
   <h2>🎉 You guessed it! 🎉</h2>
   <div style="margin-top: 20px; text-align: left; line-height: 1.6; font-size: 1.2em;">
-    <p><strong>Name:</strong> Emmett William-Chandler Stratton</p>
+    <p><strong>Name:</strong> Emmett William Stratton</p>
     <p><strong>Born:</strong> June 14th, 2025</p>
     <p><strong>Time:</strong> 3:17 AM</p>
     <p><strong>Weight:</strong> 7 lbs 1 oz</p>
